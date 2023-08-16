76791758ceb1f7cdf639df222aba4bfb9e81a246
Vitor Henrique <vitor.henrique@xds.com.br>
2023-08-16 11:26:16 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Vitor Henrique Barreiro Marinho on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
