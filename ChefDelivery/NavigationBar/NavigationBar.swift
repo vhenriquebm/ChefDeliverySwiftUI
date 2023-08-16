149037e6fb12ec7c349646f182dd56189a3649c0
Vitor Henrique <vitor.henrique@xds.com.br>
2023-08-16 12:25:54 -0300

    Criação da Navbar

---
+++ ChefDelivery/NavigationBar/NavigationBar.swift
@@ -0,0 +1,36 @@
+//
+//  NavigationBar.swift
+//  ChefDelivery
+//
+//  Created by Vitor Henrique Barreiro Marinho on 16/08/23.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        
+        HStack {
+            Spacer()
+            
+            Button("R.Vergueiro, 3185") {}
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .foregroundColor(.black)
+            Spacer()
+            
+            Button(action: {}) {  Image(systemName: "bell.badge")}
+                .foregroundColor(.red)
+                .font(.title3)
+        }
+        
+    }
+}
+
+struct NavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBar()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
